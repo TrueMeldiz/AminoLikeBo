f8ca3065a56fb9fe09269031af80c68a5b8ecb49
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-06-09 10:32:56 +0600

    Update AminoLikeBo.py

--- AminoLikeBo.py
+++ AminoLikeBo.py
@@ -1,16 +1,14 @@
+import amino
+import pyfiglet
+import concurrent.futures
 from colorama import init, Fore, Back, Style
 init()
-print(Back.BLACK)
-print(Fore.YELLOW)
-print(Style.NORMAL)
+print(Fore.YELLOW + Style.NORMAL)
 print("""Script by Lil Zevi
-Github : https://github.com/LilZevi
-▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ █░░ ▀ █░▄▀ █▀▀ █▀▄ ▄▀▄
-█▀█ █░█░█ █ █░▀█ █░█ █░▄ █ █▀▄░ █▀▀ █▀█ █░█
-▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ▀▀▀ ▀ ▀░▀▀ ▀▀▀ ▀▀░ ░▀░""")
-import amino
-email=input("Email/Почта:")
-password=input("Password/Пароль:")
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminolikebo", font="big"))
+email = input("Email/Почта: ")
+password = input("Password/Пароль: ")
 client = amino.Client()
 client.login(email=email, password=password)
 clients = client.sub_clients(start=0, size=1000)
@@ -19,17 +17,15 @@ for x, name in enumerate(clients.name, 1):
 communityid = clients.comId[int(input("Выберите сообщество/Select the community: "))-1]
 sub_client = amino.SubClient(comId=communityid, profile=client.profile)
 
-valueofblogs=int(input("Type the number of blogs/Введите количество блогов:"))
-num=0
-while valueofblogs > num and len:
-	lb=sub_client.get_recent_blogs(start=num, size=valueofblogs).blogId
-	b=0
-	for id in lb:
-		try:
-			sub_client.like_blog(blogId=id)
-			print("liked the blog/поставили лайк на блог =",id)
-			b=b+1
-		except:
-			f=True
-			num=num+25
-			pass
+numberofblogs = int(input("Type the number of blogs/Введите количество блогов: "))
+
+while True:
+	try:
+		with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+			for i in range(0, 2000, 250):
+				blogs = sub_client.get_recent_blogs(start=i, size=numberofblogs).blogId
+				for id in blogs:
+					_ = [executor.submit(sub_client.like_blog, id)]
+					print(f"{id} Liked")
+	except:
+		pass
